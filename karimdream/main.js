881f231168d67dc8de16772fc01a23f98f3396f8
KarimLeon <leon.karim08@gmail.com>
2017-09-27 15:21:19 -0400

    Update

---
+++ karimdream/main.js
@@ -0,0 +1,38 @@
+var img = document.getElementById('notification')  
+var div = document.querySelector('.Clear')
+var nav = document.getElementById('nav')
+var img2 = document.getElementById('menu')
+  
+  
+
+  img.onclick = function () {
+    var div2 = div.getAttribute('class')
+
+   switch(div2) {
+     
+     case "Clear":
+      div.setAttribute('class', 'red')
+      img.setAttribute('src', 'Images/chris2.png')
+
+      break;
+
+    case "red": 
+     div.setAttribute('class', 'Clear')
+     img.setAttribute('src', 'Images/chris.png')
+   
+    
+
+   }
+
+  };
+
+
+  $(document).ready(function() {
+     
+    $(nav).click(function () {
+        
+       $(img2).toggle();
+
+    })
+
+  })
